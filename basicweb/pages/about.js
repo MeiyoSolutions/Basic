31383d6e1ccfda980ffbebad3aee680d1404bbff
Aafthab Ashraff <aafthab.ashraff@gmail.com>
2022-09-13 09:45:31 +0100

    Add a Homepage and About page

---
+++ basicweb/pages/about.js
@@ -0,0 +1,8 @@
+const about = () => {
+    return <div>
+        <h1>About Us</h1>
+
+    </div>;
+}
+ 
+export default about;
---
+++ basicweb/pages/index.js
@@ -0,0 +1,12 @@
+import Head from 'next/head'
+import Image from 'next/image'
+import styles from '../styles/Home.module.css'
+
+export default function Home() {
+  return (
+    <div>
+     <h1>Welcome to Meiyo Solutions</h1>
+     <p1> We are a Digital Solutions Company providing our Servicies to small business to enable them to compete in this increasing digitalizing world </p1>
+    </div>
+  )
+}
